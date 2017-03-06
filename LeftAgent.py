5bf760d9add1f0499f548ea9943f7c07837a6610
Nicholas Robinson <nr124@hw.ac.uk>
2017-03-06 14:36:50 +0000

    Left Agent completed

--- LeftAgent.py
+++ LeftAgent.py
@@ -20,21 +20,27 @@ def main():
 	mr.runAgent()
 
 def frontBlock(grid, direction):
-	if (direction <= 45 or direction > 315):
-		return grid[17]
-	elif (direction <= 135 and direction > 45):
+	#if (direction <= 0.25 and direction > -0.25):
+	if (direction == 0.0):
+		return grid[16]
+	#elif (direction >= -0.75 and direction < -0.25):
+	elif (direction == 90):
 		return grid[12]
-	elif (direction <= 225 and direction > 135):
+		#elif (direction >= 0.75 and direction < -0.75):
+	elif (direction == 180.0):
 		return grid[10]
 	else:
 		return grid[14]
 
 def leftBlock(grid, direction):
-	if (direction <= 45 or direction > 315):
+	#if (direction <= 0.25 and direction > -0.25):
+	if (direction == 0.0):
 		return grid[14]
-	elif (direction <= 135 and direction > 45):
-		return grid[17]
-	elif (direction <= 225 and direction > 135):
+	#elif (direction >= -0.75 and direction < -0.25):
+	elif (direction == 90):
+		return grid[16]
+	#elif (direction >= 0.75 and direction < 0.75):
+	elif (direction == 180.0):
 		return grid[12]
 	else:
 		return grid[10]
@@ -45,40 +51,36 @@ def agentFun():
 	while world_state.is_mission_running:
 		observationsNum = world_state.number_of_observations_since_last_state
 		if observationsNum > 0:
-			#print "Got " + str(observationsNum) + " observations since last state."
+			print "Got " + str(observationsNum) + " observations since last state."
 			#print "LeftAgent: World state observations: " + str(len(world_state.observations))
-
-			observations = world_state.observations[0].text
-			#print observations
-			#direction = re.split(',', observations)[14]
-			direction = re.split(':', re.split('[\[\]]', observations)[2])[1]
-			direction = direction[:-1]
+			observations = world_state.observations[-1].text
+			print observations
+			direction = re.split(',', observations)[14]
+			direction = float(direction[6:])
 			grid = re.split(',', re.split('[\[\]]', observations)[1])
 
 			print "Left block: " + leftBlock(grid, direction)
 			print "Front block: " + frontBlock(grid, direction)
 			#print "Wall block: " + wall
-			print "Grid: " + str(grid)
-			#print "Direction: " + direction
+			#print "Grid: " + str(grid)
+			print "Direction: " + str(direction)
 			if(leftBlock(grid, direction) == wall):
-				print "Left wall detected"
+				#print "Left wall detected"
 				if(frontBlock(grid, direction) != wall):
-					print "Front wall not detected"
-					print "Moving Forward"
+					#print "Front wall not detected"
+					#print "Moving Forward"
 					mr.c.moveForward()
 				else:
-					print "Front wall detected"
+					#print "Front wall detected"
 					print "Turning Right"
-					#mr.c.turnRight()
+					mr.c.turnRight()
 			else:
-				print "Left wall not detected"
+				#print "Left wall not detected"
 				print "Turning Left"
-				print "Moving Forward"
-				#mr.c.turnLeft()
-				#mr.c.moveForward()
-			mr.c.moveForward()
+				#print "Moving Forward"
+				mr.c.turnLeft()
+				mr.c.moveForward()
 		world_state = mr.agent_host.getWorldState()
-		time.sleep(1)
 
 mr = MalmoRun()
 main()
